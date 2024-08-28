129aab975497c2f0fd5f4b60ef2887bf4c059ac1
Anuj Verdhan <114421335+anujverdhan04@users.noreply.github.com>
2024-08-28 12:33:14 +0530

    Binary_Tree_Right_Side_View(LT-199).py

--- Binary_Tree_Right_Side_View(LT-199).py
+++ Binary_Tree_Right_Side_View(LT-199).py
@@ -4,8 +4,23 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
-    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:    
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        
+        '''
+        result = []
+        def dfs(node, level):
+            if not node:
+                return
+            if level == len(result):
+                result.append(node.val)
+            dfs(node.right, level + 1)
+            dfs(node.left, level + 1)
+        
+        dfs(root, 0)
+        return result
+       '''     
         result = []
         if not root:
             return result
